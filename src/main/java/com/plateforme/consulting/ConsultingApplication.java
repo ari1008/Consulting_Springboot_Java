fa0785d4cccf8bc15b12ae8dc530bddc2300c50b
Aristide Fumo <aristide.fumo@sopth.com>
2022-12-14 23:07:41 +0100

    add file untracked

---
+++ src/main/java/com/plateforme/consulting/ConsultingApplication.java
@@ -0,0 +1,13 @@
+package com.plateforme.consulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plateforme/consulting/ConsultingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plateforme.consulting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
